b4f6ae7b4a2408db72e9b5fe08dc1dd99cbec03e
balaram gayen <balaramgayen53501@gmail.com>
2021-01-13 15:18:22 +0530

    UI design complete

--- src/components/body.js
+++ src/components/body.js
@@ -1,6 +1,12 @@
 import React, { Component } from 'react';
 import './css/body.css';
 
+// my API key : 96252dec9c05de24a0b216615e8d7777
+
+//dinesh: 8284bb913b8222a688b72546dc36f5d0
+//url: http://api.openweathermap.org/data/2.5/weather?q=kolkata&units=metric&appid=96252dec9c05de24a0b216615e8d7777
+// url: http://api.openweathermap.org/data/2.5/forecast?q=kolkata&units=metric&appid=96252dec9c05de24a0b216615e8d7777
+
 class Body extends Component {
     state = {  }
     render() { 
@@ -36,7 +42,7 @@ class Body extends Component {
                                     </div>
                                     <div className="col-md-4 col-sm-4">
                                         <img src="http://openweathermap.org/img/wn/02d@2x.png" alt="iconImage"></img>
-                                        <h5 className="text">Feels Like 27<span>&#8451;</span></h5>
+                                        <h5 className="text">Real Feel 27<span>&#8451;</span></h5>
                                     </div>
                                 </div>
                               </div>
@@ -46,17 +52,39 @@ class Body extends Component {
                     </div>
                 </div>
 
-                <div className="container mt-3 secContainer">
+                <div className="container mt-2">
                     <div className="row">
-                        <div className="col-md-12">
-                            <h2>Today Weather in <span>Kolkata</span></h2>
-                            <h1>26<span>&#8451;</span></h1>
-                            <h5>Feels Like</h5>
+                        <div className="col-md-8 col-sm-12">
+                        <div class="card secContainer">
+                            <div class="card-body">
+                                <div className="row">
+                                    <div className="col-md-12">
+                                        <h2>Today Weather in <b>Kolkata</b></h2>
+                                        <h2 className="temp2">26<span>&#8451;</span></h2>
+                                    </div>
+                                </div>
+                                <div className="row">
+                                    <div className="col-md-6 col-sm-6">
+                                        <hr></hr>
+                                        <h3><i class="fa fa-thermometer-full" aria-hidden="true"></i> Max Temperature: <span className="float"><span>26</span><span>&#8451;</span></span></h3>
+                                        <hr></hr>
+                                        <h3><i class="fa fa-thermometer-three-quarters" aria-hidden="true"></i> Min Temperature<span className="float"><span>24</span><span>&#8451;</span></span></h3>
+                                        <hr></hr>
+                                        <h3><i class="fa fa-eye" aria-hidden="true"></i> Visibility<span className="float"><span>2.5</span> km</span></h3>
+                                    </div>
+                                    <div className="col-md-6 col-sm-6">
+                                        <hr></hr>
+                                        <h3><i class="fa fa-arrow-down" aria-hidden="true"></i> Pressure<span className="float"><span>1016</span> mb</span></h3>
+                                        <hr></hr>
+                                        <h3><i class="fa fa-tint" aria-hidden="true"></i> Humidity<span className="float"><span>73</span> %</span></h3>
+                                        <hr></hr>
+                                        <h3><i class="fa fa-flag" aria-hidden="true"></i> wind<span className="float"><span>2.3</span> km/h</span></h3>
+                                    </div>
+                                </div>
+                              </div>
+                            </div>
                         </div>
-                    </div>
-                    <div className="row">
-                        <div className="col-md-6"></div>
-                        <div className="col-md-6"></div>
+                        <div className="col-md-4"></div>
                     </div>
                 </div>
             </React.Fragment>
--- src/index.js
+++ src/index.js
@@ -15,3 +15,7 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+// my API key : 96252dec9c05de24a0b216615e8d7777
+
+//dinesh: 8284bb913b8222a688b72546dc36f5d0
+//url: http://api.openweathermap.org/data/2.5/weather?q=kolkata&units=metric&appid=96252dec9c05de24a0b216615e8d7777
